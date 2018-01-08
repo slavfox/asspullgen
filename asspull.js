eadc3d0f047e68c76ae891ffbfcbe66f38968c02
Slavfox <slavfoxman@gmail.com>
2018-01-08 13:39:40 +0100

    Add crazy mode

--- asspull.js
+++ asspull.js
@@ -1,19 +1,22 @@
 var people = [
     'the judge',
     'the witness',
+    'a witness',
     'the detective',
     'the prosecutor',
     'the defense attorney',
     'the victim',
     'the defendant',
     'the killer',
+    'a detective',
+    'a prosecutor',
+    'a defense attorney',
+    'a victim',
+    'a defendant',
+    'a killer',
     'the first witness',
     'the second witness',
     'the third witness',
-    'the admin',
-    'the mod',
-    'the fourth witness',
-    'the fifth witness',
     'the bailiff',
     'Layton',
     'Greg',
@@ -23,31 +26,42 @@ var people = [
     'the co-def',
     'the support pro',
     'the support def',
-    'Verin',
-    'Rhoda',
-    'Adam',
     'Jean Descole',
     'Don Paolo',
     'Adachi',
     'the final boss',
     'Barnham',
     'Sherlock',
-    'a mafia player',
     'Komaeda',
     'Larry',
+    'Inferno Cop',
+    'a criminal',
+    'a drunk'
+]
+
+var people_crazy = [
+    'the admin',
+    'the mod',
+    'Verin',
+    'Rhoda',
+    'Adam',
+    'a mafia player',
+    'a newfriend',
+    'a vampire',
+    'a demonic entity transcending time and space',
     'God',
     'Satan',
-    'a demonic entity transcending time and space',
-    'Inferno Cop',
-    'a vampire',
-    'a criminal',
     'a clone',
     'a ghost',
-    'a lesbian',
     'a fag',
-    'a newfriend',
+    'the fourth witness',
+    'the fifth witness',
+    'a lesbian',
     'an autist',
-    'a drunk'
+    'an alien',
+    'a wizard',
+    'a witch',
+    ''
 ]
 
 var items_singular = [
@@ -83,10 +97,16 @@ var items_singular = [
     'elaborate pulley system',
     'time of death',
     'freezer',
-    'sheer force of will',
     'bullet hole',
     'testimony',
-    'cross examination'
+    'cross examination',
+]
+
+var items_crazy = [
+    'sheer force of will',
+    'verdict',
+    'steamed hams',
+    'aurora borealis',
 ]
 
 var verbs = [
@@ -100,20 +120,23 @@ var verbs = [
     'forge',
     'steal',
     'have sex with',
-    'ban',
-    'meme at',
-    'dunk on',
     'convict',
     'defend',
     'marry',
     'stab',
     'shoot',
-    'multiclient'
+    'channel'
+]
+
+var verbs_crazy = [
+    'multiclient',
+    'ban',
+    'meme at',
+    'dunk on',
 ]
 
 var items_plural = [
     'fingerprints',
-    'pulleys',
     'pulleys',
     'pieces of evidence',
     'footprints',
@@ -121,9 +144,12 @@ var items_plural = [
     'photos',
     'items',
     'banknotes',
-    'time shenanigans',
     'bullet holes',
     'testimonies'
+]
+
+var items_plural_crazy = [
+    'time shenanigans',
 ]
 
 var locations = [
@@ -143,17 +169,21 @@ var locations = [
     'prison',
     'church',
     'school',
+    'freezer'
+]
+
+var locations_crazy = [
     'discord',
     'area 1',
     'arcade',
     'google doc',
-    'freezer'
 ]
 
 var asspulls = [
     'the {item} is actually the {item}',
-    'the {item} was originally found at the {location}',
-    'the {item} is still at the {location}',
+    'the {item} was originally inside the {location}',
+    'the {item} is still inside the {location}',
+    'the {item} was not inside the {location}',
     'the {item} was never the {item}',
     'we still haven\'t found the {item}',
     'we still haven\'t found the {items}',
@@ -181,29 +211,135 @@ var asspulls = [
     '{person} wanted to {verb} the {items}',
     'if we don\'t {verb} {person} now, they will escape',
     'if we don\'t {verb} {person} now, they will {verb} again',
+    'if we don\'t {verb} {person} now, they will {verb} {person}',
     '{person} has the {item}',
     '{person} couldn\'t have been the one to {verb} {person}',
     '{person} couldn\'t have been the one to {verb} the {item}',
     'the victim was killed with {person}\'s {item}',
-    'the {item} crashed the server',
     'the {item} was used by {person}',
     'the {item} was used to throw off the time of death',
-    '{person} is a clone',
     '{person} was framed by {person}',
     'the defendant was framed by {person}',
     'the {item} is fake',
     'the {item} has no relevance to the case',
+    'I am actually {person}',
+    'the {item} belongs to me'
+]
+
+var asspulls_crazy = [
+    '{person} is a clone',
+    'the {item} crashed the server',
+    '{person} is multiclienting'
 ]
 
 function get_random(arr){
     return arr[Math.floor(Math.random()*arr.length)];
 }
 
-function asspullify(sentence){
+function crazify_people(){
+    var new_people = people.slice();
+    for(var i = 0; i<items_singular.length; i++){
+        new_people.push("the " + items_singular[i]);
+    }
+    for(var i = 0; i<locations.length; i++){
+        new_people.push("the " + locations[i]);
+    }
+    new_people = new_people.concat(people_crazy);
+    return new_people;
+}
+
+var crazy_people = crazify_people();
+
+function crazify_items_s(){
+    var new_items_s = items_singular.slice();
+    for(var i = 0; i<people.length; i++){
+        new_items_s.push(
+            people[i].replace(
+                new RegExp("^the "), 
+                ""
+            ).replace(
+                new RegExp("^a "),
+                ""
+            ).replace(
+                new RegExp("^an "),
+                "")
+        );
+    }
+    new_items_s = new_items_s.concat(items_crazy);
+    new_items_s = new_items_s.concat(locations);
+    return new_items_s;
+}
+
+var crazy_items_s = crazify_items_s();
+
+function crazify_items_p(){
+    var new_items_p = items_plural.slice();
+    new_items_p = new_items_p.concat(items_plural_crazy);
+    return new_items_p;
+}
+
+var crazy_items_p = crazify_items_p();
+
+function crazify_locations(){
+    var new_locations = locations.slice();
+    for(var i = 0; i<people.length; i++){
+        new_locations.push(
+            people[i].replace(
+                new RegExp("^the "), 
+                ""
+            ).replace(
+                new RegExp("^a "),
+                ""
+            ).replace(
+                new RegExp("^an "),
+                "")
+        );
+    }
+    new_locations = new_locations.concat(crazy_items_s);
+    new_locations = new_locations.concat(locations_crazy);
+    return new_locations;
+}
+
+var crazy_locations = crazify_locations();
+
+function crazify_verbs(){
+    var new_verbs = verbs.slice();
+    new_verbs = new_verbs.concat(verbs_crazy);
+    return new_verbs;
+}
+
+var crazy_verbs = crazify_verbs();
+
+function crazify_asspulls(){
+    var new_asspulls = asspulls.slice();
+    new_asspulls = new_asspulls.concat(asspulls_crazy);
+    return new_asspulls;
+}
+
+var crazy_asspulls = crazify_asspulls();
+
+
+function asspull() {
+    if (document.getElementById("crazymode").checked){
+        var items_singular_ = crazy_items_s;
+        var items_plural_ = crazy_items_p;
+        var people_ = crazy_people;
+        var locations_ = crazy_locations;
+        var verbs_ = crazy_verbs;
+        var asspulls_ = crazy_asspulls;
+    } else {
+        var items_singular_ = items_singular;
+        var items_plural_ = items_plural;
+        var people_ = people;
+        var locations_ = locations;
+        var verbs_ = verbs;
+        var asspulls_ = asspulls;
+    }
+    var sentence = get_random(asspulls_);
     while (sentence.indexOf("{item}") !== -1) {
-        var item = get_random(items_singular);
+        var item = get_random(items_singular_);
         if (Math.random() < 0.1){
-            item = get_random(people) + "'s " + item;
+            item = get_random(people_) + "'s " + item;
             if (sentence.indexOf("the {item}") !== -1) {
                 sentence = sentence.replace("the {item}", item);
             } else{
@@ -214,25 +350,21 @@ function asspullify(sentence){
         }
     }
     while (sentence.indexOf("{items}") !== -1) {
-        var items_p = get_random(items_plural);
+        var items_p = get_random(items_plural_);
         sentence = sentence.replace("{items}", items_p);
     }
     while (sentence.indexOf("{person}") !== -1) {
-        var person = get_random(people);
+        var person = get_random(people_);
         sentence = sentence.replace("{person}", person);
     }
     while (sentence.indexOf("{location}") !== -1) {
-        var location = get_random(locations);
+        var location = get_random(locations_);
         sentence = sentence.replace("{location}", location);
     }
     while (sentence.indexOf("{verb}") !== -1) {
-        var verb = get_random(verbs);
+        var verb = get_random(verbs_);
         sentence = sentence.replace("{verb}", verb);
     }
-    return sentence.charAt(0).toUpperCase() + sentence.slice(1) + "!";
-}
-
-function asspull() {
-    var sentence = get_random(asspulls);
-    document.getElementById("asspull").innerHTML = asspullify(sentence);
+    sentence = sentence.charAt(0).toUpperCase() + sentence.slice(1) + "!";
+    document.getElementById("asspull").innerHTML = sentence;
 }
